9a2ab5f4c75e324878dcf89d772dddd5cde68b1e
NeilKingdom <neil@neilkingdom.xyz>
2023-10-30 23:23:29 -0400

    Cube renders correctly. User input can move camera

--- src/window.cpp
+++ src/window.cpp
@@ -6,13 +6,16 @@
 #include <string>
 #include <chrono>
 #include <algorithm>
+#include <iomanip>
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/keysymdef.h>
 
-#include <GL/glew.h> // Put before other OpenGL library headers
+#include <GL/glew.h> // NOTE: Put before other OpenGL library headers
 #include <GL/gl.h>
 #include <GL/glx.h>  // X11-specific APIs
 
@@ -21,31 +24,36 @@
 
 #define APP_TITLE "KingCraft"
 
+using namespace std::chrono;
+
 // X11 variables
 Display                *dpy;  // The target monitor/display
 Window                  win;  // The application's window
 Screen                 *scrn; //
 int                     scrn_id;
 XVisualInfo            *vi;   // Struct containing additional info about the window
-XWindowAttributes       gwa;  // Get attributes struct
-XEvent                  xev;  // XEvent struct for handling X events
+XWindowAttributes       gwa;  // Get window attributes struct
+XEvent                  xev;  // XEvent stores the most-recently received event type
 GLXContext              glx;  // The OpenGL context for X11
 
-using namespace std::chrono;
-void CalculateFrameRate(int &fps, int &_fpsCount, steady_clock::time_point &lastTime)
+static float fov = lac_deg_to_rad(90.0f);
+static float znear = 1.0f;
+static float zfar = 1000.0f;
+
+void CalculateFrameRate(int &fps, int &fps_inc, steady_clock::time_point &time_prev)
 {
-    auto currentTime = steady_clock::now();
+    auto time_now = steady_clock::now();
+    auto time_elapsed = duration_cast<nanoseconds>(time_now - time_prev).count();
+    constexpr long sec_as_nano = 1 * 1000 * 1000 * 1000;
+    ++fps_inc; // Increment each frame
 
-    const auto elapsedTime = duration_cast<nanoseconds>(currentTime - lastTime).count();
-    ++_fpsCount;
-
-    if (elapsedTime > 1000000000)
+    if (time_elapsed > sec_as_nano) // Wait until at least a second has elapsed
     {
-        lastTime = currentTime;
-        fps = _fpsCount;
-        _fpsCount = 0;
+        time_prev = time_now;
+        fps = fps_inc;
+        fps_inc = 0;
 
-        std::cout << "FPS: " << fps << std::endl; // print out fps in every second (or you can use it elsewhere)
+        std::cout << "FPS: " << fps << std::endl;
     }
 }
 
@@ -77,7 +85,7 @@ static unsigned CompileShader(unsigned type, const std::string source)
 
 static unsigned CreateShader(const std::string vertexShader, const std::string fragmentShader)
 {
-   unsigned int program = glCreateProgram(); // Create a shader program
+   unsigned int program = glCreateProgram();
    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
@@ -93,25 +101,21 @@ static unsigned CreateShader(const std::string vertexShader, const std::string f
 }
 
 static bool isExtensionSupported(const char *extList, const char *extension) {
-	const char *start;
-	const char *where, *terminator;
+	const char *start, *where, *terminator;
 
 	where = strchr(extension, ' ');
 	if (where || *extension == '\0') {
 		return false;
 	}
 
-	for (start=extList;;) {
+   start=extList;
+   while (true) {
 		where = strstr(start, extension);
-
-		if (!where) {
-		 	break;
-		}
+		if (!where) break;
 
 		terminator = where + strlen(extension);
-
-		if ( where == start || *(where - 1) == ' ' ) {
-			if ( *terminator == ' ' || *terminator == '\0' ) {
+		if (where == start || *(where - 1) == ' ') {
+			if (*terminator == ' ' || *terminator == '\0') {
 				return true;
 			}
 		}
@@ -128,8 +132,7 @@ int main(int argc, char **argv)
 
    // Open the display
    dpy = XOpenDisplay(NULL); // NULL = first monitor
-   if (dpy == NULL)
-   {
+   if (dpy == NULL) {
       std::cerr << "Cannot connect to X server" << std::endl;
       exit(-1);
    }
@@ -166,12 +169,12 @@ int main(int argc, char **argv)
    GLXFBConfig *fbc = glXChooseFBConfig(dpy, win, glx_attribs, &fbcount);
    if (fbc == NULL)
    {
-      std::cerr << "Failed to retrieve framebuffer\n" << std::endl;
+      std::cerr << "Failed to retrieve framebuffer" << std::endl;
       XCloseDisplay(dpy);
-      exit(-1);
+      exit(EXIT_FAILURE);
    }
 
-   // Pick the FB config/visual with the most samples per pixel
+   // Pick the FB config/visual with the most samples per-pixel
 	std::cout << "Getting best XVisualInfo\n";
 	int best_fbc = -1, worst_fbc = -1, best_num_samp = -1, worst_num_samp = 999;
 	for (int i = 0; i < fbcount; i++) {
@@ -196,8 +199,7 @@ int main(int argc, char **argv)
 	XFree(fbc);
 
    vi = glXGetVisualFromFBConfig(dpy, bestFbc);
-   if (vi == NULL)
-   {
+   if (vi == NULL) {
       std::cerr << "No appropriate visual found" << std::endl;
       exit(-1);
    }
@@ -206,18 +208,19 @@ int main(int argc, char **argv)
       exit(-1);
    }
 
-   // Open the window
+   /*** Open an X11 window ***/
+
 	XSetWindowAttributes windowAttribs;
 	windowAttribs.border_pixel = BlackPixel(dpy, scrn_id);
 	windowAttribs.background_pixel = WhitePixel(dpy, scrn_id);
 	windowAttribs.override_redirect = true;
 	windowAttribs.colormap = XCreateColormap(dpy, RootWindow(dpy, scrn_id), vi->visual, AllocNone);
-	windowAttribs.event_mask = ExposureMask;
-	win = XCreateWindow(dpy, RootWindow(dpy, scrn_id), 0, 0, 600, 600, 0, vi->depth, InputOutput, vi->visual, CWBackPixel | CWColormap | CWBorderPixel | CWEventMask, &windowAttribs);
+	windowAttribs.event_mask = (ExposureMask | KeyPressMask | ButtonPressMask | ButtonReleaseMask);
+	win = XCreateWindow(dpy, RootWindow(dpy, scrn_id), 0, 0, 600, 600, 0, vi->depth, InputOutput, vi->visual, (CWBackPixel | CWColormap | CWBorderPixel | CWEventMask), &windowAttribs);
 
-	// Create GLX OpenGL context
-	glXCreateContextAttribsARBProc glXCreateContextAttribsARB = 0;
-	glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc) glXGetProcAddressARB( (const GLubyte *) "glXCreateContextAttribsARB" );
+	/*** Create GLX OpenGL context ***/
+
+	glXCreateContextAttribsARBProc glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)glXGetProcAddressARB((const GLubyte *)"glXCreateContextAttribsARB");
 
 	const char *glxExts = glXQueryExtensionsString(dpy, scrn_id);
 	std::cout << "Late extensions:\n\t" << glxExts << std::endl;
@@ -248,20 +251,16 @@ int main(int argc, char **argv)
    glXMakeCurrent(dpy, win, glx);
 
 #ifdef DEBUG
-   std::cout << "GL Vendor: " << glGetString(GL_VENDOR) << "\n";
-	std::cout << "GL Renderer: " << glGetString(GL_RENDERER) << "\n";
-	std::cout << "GL Version: " << glGetString(GL_VERSION) << "\n";
-	std::cout << "GL Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
+   std::cout << "GL Vendor: " << glGetString(GL_VENDOR) << std::endl;
+	std::cout << "GL Renderer: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "GL Version: " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "GL Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 #endif
-
-   // TODO: Enable events
-   // Handle the following events:
-   //swa.event_mask = (ExposureMask | KeyPressMask | ButtonPressMask | ButtonReleaseMask);
 
    XClearWindow(dpy, win);
    XMapRaised(dpy, win);
 
-   // Must be placed after a valid OpenGL context has been made current
+   // NOTE: Must be placed after a valid OpenGL context has been made current
    if (glewInit() != GLEW_OK)
    {
       std::cerr << "Failed to initialize GLEW" << std::endl;
@@ -271,33 +270,53 @@ int main(int argc, char **argv)
    /*** Setup debugging ***/
 
    // NOTE: You cannot OR these!!!
-   glEnable(GL_DEPTH_TEST);
+   glEnable(GL_DEPTH_TEST); // Enable z-ordering via depth buffer
+   glEnable(GL_CULL_FACE); // Cull faces which are not visible to the camera
+   glDepthFunc(GL_LESS); // Culling algorithm (GL_LESS = lower zbuffer values are rendered on top)
+   glCullFace(GL_FRONT); // Use if triangles are rasterized in clockwise ordering
    glEnable(GL_DEBUG_OUTPUT);
    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
    if (glDebugMessageCallback)
       glDebugMessageCallback(debugCallback, nullptr); // Set callback function for debug messages
    else
-      std::cout << "WARNING: glDebugMessageCallback() is unavailable!" << std::endl;
+      std::cerr << "WARNING: glDebugMessageCallback() is unavailable!" << std::endl;
 
    /*** Setup VAO, VBO, and EBO ***/
 
    float vertices[] = {
-       // positions         // colors
-       0.5f,  0.5f,  0.0f,  0.0f, 0.0f, 1.0f,   // top right
-       0.5f, -0.5f,  0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-      -0.5f, -0.5f,  0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-      -0.5f,  0.5f,  0.0f,  0.5f, 0.5f, 0.5f    // top left
+       // positions          // colors
+       0.5f,  0.5f,  -0.5f,  0.3f, 0.7f, 0.6f,  // top right (front)
+       0.5f, -0.5f,  -0.5f,  1.0f, 1.0f, 0.9f,  // bottom right (front)
+      -0.5f, -0.5f,  -0.5f,  0.4f, 1.0f, 0.3f,  // bottom left (front)
+      -0.5f,  0.5f,  -0.5f,  0.7f, 0.7f, 1.0f,  // top left (front)
+
+       0.5f,  0.5f,   0.5f,  0.9f, 0.5f, 0.1f,  // top right (back)
+       0.5f, -0.5f,   0.5f,  1.0f, 0.4f, 0.2f,  // bottom right (back)
+      -0.5f, -0.5f,   0.5f,  0.2f, 1.0f, 0.3f,  // bottom left (back)
+      -0.5f,  0.5f,   0.5f,  0.5f, 0.5f, 0.5f   // top left (back)
    };
 
    /*
-    * 3--0
-    * |\ |
-    * | \|
-    * 2--1
+    *   7____4
+    *  /|   /|
+    * 3-+--0 |
+    * | 6__|_5
+    * |/   |/
+    * 2----1
     */
-   unsigned int indices[6] = {
-      0, 1, 3,
-      1, 2, 3
+   unsigned int indices[] = {
+      3, 0, 2,
+      0, 1, 2,
+      7, 4, 3,
+      4, 0, 3,
+      6, 5, 7,
+      5, 4, 7,
+      2, 1, 6,
+      1, 5, 6,
+      0, 4, 1,
+      4, 5, 1,
+      7, 3, 6,
+      3, 2, 6
    };
 
    unsigned int vao, vbo, ebo;
@@ -342,30 +361,39 @@ int main(int argc, char **argv)
 
    unsigned int shader = CreateShader(vertexShader, fragmentShader);
 
+   /*** Initialize some variables ***/
+
+   float camera_trn_x = 0.0f;
+
    float theta_rad = 0.0f;
    float theta_deg = 0.0f;
-   float t_fact = -2.0f;
+   float trn_fact = -3.0f;
 
    mat4 trn_mat = { 0 };
    mat4 rot_mat = { 0 };
-   mat4 model = { 0 };
+   mat4 model_mat = { 0 };
+   mat4 view_mat = { 0 };
+   mat4 proj_mat = { 0 };
 
-   int _fpsCount = 0;
-   int fps = 0; // this will store the final fps for the last second
+   int fps_inc = 0;
+   int fps = 0;
 
-   using namespace std::chrono;
-   time_point<steady_clock> lastTime = steady_clock::now();
+   time_point<steady_clock> time_prev = steady_clock::now();
+   std::chrono::duration<long, std::ratio<1, 1000000>>::rep time_elapsed;
 
    /*** Game loop ***/
 
    while (true)
    {
+      auto time_prev2 = steady_clock::now();
+
       /*** Process events ***/
 
       while (XPending(dpy) > 0)
       {
          XNextEvent(dpy, &xev); // Blocks until event is received
 
+         // TODO: Programmatically set keyboard delay via X11 akin to xset (reduces choppiness)
          switch (xev.type)
          {
             case Expose: // Window was being overlapped by another window, but is now exposed
@@ -373,13 +401,24 @@ int main(int argc, char **argv)
                /* Set affine transform for viewport based on window width/height */
                XGetWindowAttributes(dpy, win, &gwa);
                glViewport(0, 0, gwa.width, gwa.height);
-
-               std::cout << "Window was exposed" << std::endl;
                break;
             }
-            case KeyPress:
+            case (KeyPress | KeyRelease):
             {
-               std::cout << "Key press detected" << std::endl;
+               KeySym sym = XLookupKeysym(&xev.xkey, 0);
+               switch (sym)
+               {
+                  case XK_a:
+                  {
+                     camera_trn_x += 0.006 * time_elapsed;
+                     break;
+                  }
+                  case XK_d:
+                  {
+                     camera_trn_x -= 0.006 * time_elapsed;
+                     break;
+                  }
+               }
                break;
             }
             case ButtonPress: // Mouse button pressed
@@ -400,38 +439,39 @@ int main(int argc, char **argv)
 
       theta_deg += (1 % 360);
       theta_rad = lac_deg_to_rad(theta_deg);
-      t_fact -= 0.01;
+      //t_fact -= 0.01;
 
-      lac_get_rotation_mat4(&rot_mat, (theta_rad * 0.5), (theta_rad * 0.25), theta_rad);
-      lac_get_translation_mat4(&trn_mat, 0.0f, 0.0f, t_fact);
-      lac_dot_prod_mat4(trn_mat, rot_mat, &model);
+      // Model matrix (translate to world space)
+      lac_get_rotation_mat4(&rot_mat, theta_rad, (theta_rad * 0.9f), (theta_rad * 0.8f)); // Rotate
+      lac_get_translation_mat4(&trn_mat, 0.0f, 0.0f, trn_fact); // Translate
+      lac_dot_prod_mat4(trn_mat, rot_mat, &model_mat);
 
-      // Model matrix
       int modelLocation = glGetUniformLocation(shader, "model");
-      // Change to GL_TRUE if column-major ordering is required
-      glUniformMatrix4fv(modelLocation, 1, GL_TRUE, model);
+      glUniformMatrix4fv(modelLocation, 1, GL_TRUE, model_mat);
 
-      // View matrix
-      mat4 view_mat = { 0 };
-      lac_get_view_mat4(
-            &view_mat,
-            (const vec3){ 0.000001f, 0.000001f, 1.0f }, // Components must be non-zero
-            (const vec3){ 0.0f, 0.0f, 0.0f },
-            (const vec3){ 0.0f, 0.0f, 1.0f }
+      // TODO: Add inverse function to make camera behave properly
+      // View matrix (translate to view space)
+      lac_get_look_at_mat4(
+         &view_mat,
+         (const vec3){ 0.000001f, 0.000001f, 1.0f },
+         (const vec3){ 0.0f, 0.0f, 0.0f },
+         (const vec3){ 0.0f, 0.0f, 1.0f }
       );
+      view_mat[3] = camera_trn_x;
       int viewLocation = glGetUniformLocation(shader, "view");
       glUniformMatrix4fv(viewLocation, 1, GL_TRUE, view_mat);
 
       // Projection matrix
-      mat4 proj_mat = { 0 };
-      lac_get_projection_mat4(&proj_mat, ((float)gwa.height / (float)gwa.width), lac_deg_to_rad(90.0f), 1.0f, 100.0f);
+      lac_get_projection_mat4(&proj_mat, ((float)gwa.height / (float)gwa.width), fov, znear, zfar);
       int projLocation = glGetUniformLocation(shader, "proj");
       glUniformMatrix4fv(projLocation, 1, GL_TRUE, proj_mat);
 
-      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
       glXSwapBuffers(dpy, win);
 
-      CalculateFrameRate(fps, _fpsCount, lastTime);
+      auto time_now = steady_clock::now();
+      time_elapsed = duration_cast<milliseconds>(time_now - time_prev2).count();
+      //CalculateFrameRate(fps, fps_inc, time_prev);
    }
 
    glDeleteVertexArrays(1, &vao);
